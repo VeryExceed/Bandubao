8521b862ff8ca833035877f8ffbc5d4f882d2782
luomiao <1084459061@qq.com>
2020-08-23 06:01:33 +0800

    02-首页去上课链接+学习进度条

--- Bandubao/pages/index/index.vue
+++ Bandubao/pages/index/index.vue
@@ -1,8 +1,12 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<image class="go-learn" src="@/static/img/firstpage/golearn.png" @tap="gotoTeach"></image>
+		<view class="progress-box">
+			<progress :percent="learnTimePercent" activeColor="#FD7D64" backgroundColor="#F8DFDB" active stroke-width="8" />
+			<view class="progerss-desc">
+				<text>今日学习0分钟</text>
+				<text>目标10分钟</text>
+			</view>
 		</view>
 	</view>
 </template>
@@ -11,42 +15,46 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				learnTimePercent: 80
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			gotoTeach() {
+				uni.switchTab({
+					url: "../bestTeach/bestTeach"
+				})
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
 	.content {
 		display: flex;
 		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+		padding: 20rpx;
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
+		>.go-learn {
+			width: 100%;
+			border-ridius: 20rpx;
+		}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		>.progress-box {
+			margin-top: 20rpx;
+			padding: 10rpx 20rpx;
+			border-radius: 20rpx;
+			background-color: #F7F8FA;
+			.progerss-desc {
+				display: flex;
+				justify-content: space-between;
+				text {
+					font-size: 25rpx;
+					color: #666666;
+				}
+			}
+		}
 	}
 </style>
